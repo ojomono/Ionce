e113e9947facdbaefd4541f94302dd0841cd42d7
omerlevy1248 <omerlevy1248g@gmail.com>
2020-07-13 12:13:17 +0300

    Add Email link authentication

--- app/src/main/java/com/ojomono/ionce/ui/splashscreen/SplashActivity.kt
+++ app/src/main/java/com/ojomono/ionce/ui/splashscreen/SplashActivity.kt
@@ -2,16 +2,17 @@ package com.ojomono.ionce.ui.splashscreen
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
-import com.google.firebase.auth.FirebaseAuth
+import com.firebase.ui.auth.util.ExtraConstants
+import com.google.firebase.auth.ActionCodeSettings
 import com.ojomono.ionce.MainActivity
 import com.ojomono.ionce.R
 import com.ojomono.ionce.utils.BaseActivity
 import com.ojomono.ionce.utils.Constants
+
 
 class SplashActivity : BaseActivity() {
 
@@ -42,7 +43,8 @@ class SplashActivity : BaseActivity() {
                 // If response is null the user canceled the sign-in flow using the back button.
                 if (response == null) finish()
 
-                // Otherwise check response.getError().getErrorCode() and handle the error.
+                // Otherwise an error occurred
+                // TODO check response.getError().getErrorCode() and handle the error.
                 else Log.e(TAG, response.error.toString())
 
             }
@@ -51,10 +53,29 @@ class SplashActivity : BaseActivity() {
 
     private fun startSignInActivity(){
 
+        // Start building the sign-in Intent builder
+        val signInIntentBuilder = AuthUI.getInstance().createSignInIntentBuilder()
+
+        // If AuthUI can handle the intent (email link clicked), state it in the sign-in Intent
+        if (AuthUI.canHandleIntent(intent)) {
+            if (intent.extras == null) return
+            val link = intent.data.toString()
+            signInIntentBuilder.setEmailLink(link)
+        }
+
+        // Enable email link sign in
+        val actionCodeSettings : ActionCodeSettings = ActionCodeSettings.newBuilder()
+            .setAndroidPackageName(packageName, true, null)
+            .setHandleCodeInApp(true) // This must be set to true
+            .setUrl(Constants.DL_EMAIL_LINK_SIGN_IN) // This URL needs to be whitelisted
+            .build()
+
         // TODO add phone, google, facebook and twitter login support
         // Choose authentication providers
         val providers = arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder().build()//,
+            AuthUI.IdpConfig.EmailBuilder()
+                .enableEmailLinkSignIn()
+                .setActionCodeSettings(actionCodeSettings).build()//,
 //            AuthUI.IdpConfig.PhoneBuilder().build(),
 //            AuthUI.IdpConfig.GoogleBuilder().build(),
 //            AuthUI.IdpConfig.FacebookBuilder().build(),
@@ -63,12 +84,8 @@ class SplashActivity : BaseActivity() {
 
         // Create and launch sign-in intent
         startActivityForResult(
-            AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build(),
+            signInIntentBuilder.setAvailableProviders(providers).build(),
             Constants.RC_SIGN_IN)
-
     }
 
     private fun startMainActivity(){
--- app/src/main/java/com/ojomono/ionce/utils/Constants.kt
+++ app/src/main/java/com/ojomono/ionce/utils/Constants.kt
@@ -5,4 +5,7 @@ object Constants {
     // Request Codes
     const val RC_SIGN_IN = 1
 
+    // Dynamic Links
+    const val DL_EMAIL_LINK_SIGN_IN = "https://ionce.page.link"
+
 }
