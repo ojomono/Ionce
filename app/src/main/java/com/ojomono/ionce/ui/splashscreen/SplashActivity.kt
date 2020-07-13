55f226d2ed258eb97cce8ea4766019cac6ed6dca
omerlevy1248 <omerlevy1248g@gmail.com>
2020-07-13 16:53:12 +0300

    Move all Firebase usages to FirebaseProxy Object

--- app/src/main/java/com/ojomono/ionce/ui/splashscreen/SplashActivity.kt
+++ app/src/main/java/com/ojomono/ionce/ui/splashscreen/SplashActivity.kt
@@ -3,32 +3,30 @@ package com.ojomono.ionce.ui.splashscreen
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
-import com.firebase.ui.auth.util.ExtraConstants
-import com.google.firebase.auth.ActionCodeSettings
+import androidx.appcompat.app.AppCompatActivity
 import com.ojomono.ionce.MainActivity
 import com.ojomono.ionce.R
-import com.ojomono.ionce.utils.BaseActivity
 import com.ojomono.ionce.utils.Constants
+import com.ojomono.ionce.utils.FirebaseProxy
 
 
-class SplashActivity : BaseActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         // Open sign-in screen
-        startSignInActivity()
+        startActivityForResult(
+            FirebaseProxy.buildSignInIntent(packageName, intent),
+            Constants.RC_SIGN_IN
+        )
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == Constants.RC_SIGN_IN) {
-            val response = IdpResponse.fromResultIntent(data)
 
             if (resultCode == Activity.RESULT_OK) {
 
@@ -38,54 +36,8 @@ class SplashActivity : BaseActivity() {
                 // Open home screen
                 startMainActivity()
 
-            } else {    // Sign in failed.
-
-                // If response is null the user canceled the sign-in flow using the back button.
-                if (response == null) finish()
-
-                // Otherwise an error occurred
-                // TODO check response.getError().getErrorCode() and handle the error.
-                else Log.e(TAG, response.error.toString())
-
-            }
+            } else if (FirebaseProxy.handleSignInFailed(data)) finish()    // Sign in failed.
         }
-    }
-
-    private fun startSignInActivity(){
-
-        // Start building the sign-in Intent builder
-        val signInIntentBuilder = AuthUI.getInstance().createSignInIntentBuilder()
-
-        // If AuthUI can handle the intent (email link clicked), state it in the sign-in Intent
-        if (AuthUI.canHandleIntent(intent)) {
-            if (intent.extras == null) return
-            val link = intent.data.toString()
-            signInIntentBuilder.setEmailLink(link)
-        }
-
-        // Enable email link sign in
-        val actionCodeSettings : ActionCodeSettings = ActionCodeSettings.newBuilder()
-            .setAndroidPackageName(packageName, true, null)
-            .setHandleCodeInApp(true) // This must be set to true
-            .setUrl(Constants.DL_EMAIL_LINK_SIGN_IN) // This URL needs to be whitelisted
-            .build()
-
-        // TODO add phone, google, facebook and twitter login support
-        // Choose authentication providers
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder()
-                .enableEmailLinkSignIn()
-                .setActionCodeSettings(actionCodeSettings).build()//,
-//            AuthUI.IdpConfig.PhoneBuilder().build(),
-//            AuthUI.IdpConfig.GoogleBuilder().build(),
-//            AuthUI.IdpConfig.FacebookBuilder().build(),
-//            AuthUI.IdpConfig.TwitterBuilder().build()
-        )
-
-        // Create and launch sign-in intent
-        startActivityForResult(
-            signInIntentBuilder.setAvailableProviders(providers).build(),
-            Constants.RC_SIGN_IN)
     }
 
     private fun startMainActivity(){
--- app/src/main/java/com/ojomono/ionce/utils/Tagged.kt
+++ app/src/main/java/com/ojomono/ionce/utils/Tagged.kt
@@ -1,14 +1,12 @@
 package com.ojomono.ionce.utils
 
-import androidx.appcompat.app.AppCompatActivity
-
-open class BaseActivity : AppCompatActivity() {
-
-    // This extension allows us to use TAG in any class (used for logging)
+/**
+ * This extension allows us to use TAG in any class (used for logging).
+ */
+interface Tagged {
     val Any.TAG: String
         get() {
             val tag = javaClass.simpleName
             return if (tag.length <= 23) tag else tag.substring(0, 23)
         }
-
 }
