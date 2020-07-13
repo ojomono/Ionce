e25b4b137c170cb7f193f861658f6fa59c3ed229
omerlevy1248 <omerlevy1248g@gmail.com>
2020-07-13 20:57:41 +0300

    Add sign out functionality

--- app/src/main/java/com/ojomono/ionce/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/ojomono/ionce/ui/profile/ProfileViewModel.kt
@@ -3,11 +3,12 @@ package com.ojomono.ionce.ui.profile
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ojomono.ionce.utils.FirebaseProxy
 
 class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is profile Fragment"
+        value = FirebaseProxy.getCurrentUser()?.email
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/ojomono/ionce/utils/FirebaseProxy.kt
+++ app/src/main/java/com/ojomono/ionce/utils/FirebaseProxy.kt
@@ -1,12 +1,15 @@
 package com.ojomono.ionce.utils
 
+import android.content.Context
 import android.content.Intent
 import android.util.Log
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.ActionCodeSettings
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.ojomono.ionce.ui.splashscreen.SplashActivity
 
 /**
  * Handles all interactions with Firebase.
@@ -85,6 +88,16 @@ object FirebaseProxy : Tagged {
      */
     fun getCurrentUser() : FirebaseUser? {
         return FirebaseAuth.getInstance().currentUser
+    }
+
+    /**
+     * sign out of Firebase Authentication as well as all social identity providers. a [context] and
+     * a [onCompleteListener] are needed.
+     */
+    fun signOut(context: Context, onCompleteListener: OnCompleteListener<Void>) {
+        AuthUI.getInstance()
+            .signOut(context)
+            .addOnCompleteListener(onCompleteListener)
     }
 
 }
