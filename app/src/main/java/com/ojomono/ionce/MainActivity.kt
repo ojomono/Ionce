7156e3b67fe4eeadd3ced9e82faec614951acbdd
omerlevy1248 <omerlevy1248g@gmail.com>
2020-07-22 15:57:11 +0300

    Replace Default DashboardFragment.kt with TalesListFragment.kt (list fragment template)

--- app/src/main/java/com/ojomono/ionce/MainActivity.kt
+++ app/src/main/java/com/ojomono/ionce/MainActivity.kt
@@ -18,8 +18,11 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_profile))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_tales, R.id.navigation_profile
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
