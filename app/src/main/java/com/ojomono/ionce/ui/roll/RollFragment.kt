e337664d5215e65d9b723559dd877b473efa50b8
omerlevy1248 <omerlevy1248g@gmail.com>
2020-08-09 18:12:12 +0300

    Basic design for fragment_roll.xml

--- app/src/main/java/com/ojomono/ionce/ui/roll/RollFragment.kt
+++ app/src/main/java/com/ojomono/ionce/ui/roll/RollFragment.kt
@@ -22,10 +22,10 @@ class RollFragment : Fragment() {
         rollViewModel =
             ViewModelProvider(this).get(RollViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_roll, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        rollViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+//        val textView: TextView = root.findViewById(R.id.text_home)
+//        rollViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
         return root
     }
 }
