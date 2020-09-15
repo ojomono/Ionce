3afe61386bb572934689bde7ba56d049a3694345
omerlevy1248 <omerlevy1248g@gmail.com>
2020-09-15 14:19:58 +0300

    Show user's profile picture

--- app/src/main/java/com/ojomono/ionce/ui/profile/ProfileBindingUtils.kt
+++ app/src/main/java/com/ojomono/ionce/ui/profile/ProfileBindingUtils.kt
@@ -1,19 +1,32 @@
 package com.ojomono.ionce.ui.profile
 
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseUser    // TODO avoid importing firebase packages here
 import com.google.firebase.auth.TwitterAuthProvider
+import com.ojomono.ionce.R
 
-@BindingAdapter("twitterText")
-fun TextView.setTwitterText(user: FirebaseUser?) {
+@BindingAdapter("userPhotoSrc")
+fun ImageView.setUserPhotoSrc(user: FirebaseUser?) {
+    Glide.with(context)
+        .load(user?.photoUrl)
+        .circleCrop()
+        .placeholder(R.drawable.ic_profile_grey_72)
+        .fallback(R.drawable.ic_profile_grey_72)
+        .into(this)
+}
+
+@BindingAdapter("twitterLinkText")
+fun TextView.setTwitterLinkText(user: FirebaseUser?) {
     text =
         user?.providerData?.find { it.providerId == TwitterAuthProvider.PROVIDER_ID }?.displayName
 }
 
-@BindingAdapter("facebookText")
-fun TextView.setFacebookText(user: FirebaseUser?) {
+@BindingAdapter("facebookLinkText")
+fun TextView.setFacebookLinkText(user: FirebaseUser?) {
     text =
         user?.providerData?.find { it.providerId == FacebookAuthProvider.PROVIDER_ID }?.displayName
 }
