44a7dfb18887abc6963470c0ad20a0d10bf2670f
omerlevy1248 <omerlevy1248g@gmail.com>
2020-10-16 01:27:19 +0300

    Add plus/delete icons near the providers in profile screen

--- app/src/main/java/com/ojomono/ionce/ui/profile/ProfileBindingUtils.kt
+++ app/src/main/java/com/ojomono/ionce/ui/profile/ProfileBindingUtils.kt
@@ -4,6 +4,7 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseUser    // TODO avoid importing firebase packages here
+import com.google.firebase.auth.UserInfo
 import com.ojomono.ionce.R
 
 @BindingAdapter("userPhotoSrc")
@@ -13,4 +14,19 @@ fun ImageView.setUserPhotoSrc(user: FirebaseUser?) {
         .circleCrop()
         .fallback(R.drawable.ic_profile_black_24)
         .into(this)
+}
+
+@BindingAdapter("providerData", "providerNameResId")
+fun ImageView.setProviderIconSrcAndDescription(userInfo: UserInfo?, providerName: String) {
+    val imageResource: Int
+    val contentDescriptionResource: Int
+    if (userInfo == null) {
+        imageResource = R.drawable.ic_add_black_24
+        contentDescriptionResource = R.string.profile_providers_add_content_description
+    } else {
+        imageResource = R.drawable.ic_delete_black_24
+        contentDescriptionResource = R.string.profile_providers_delete_content_description
+    }
+    setImageResource(imageResource)
+    contentDescription = context.getString(contentDescriptionResource, providerName)
 }
